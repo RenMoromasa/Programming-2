503bb05a55f8ec3836bfb4b0a84bdce7ff6d791e
RenMoromasa <146726008+RenMoromasa@users.noreply.github.com>
2024-09-11 10:57:02 +0800

    Add files via upload

---
+++ 8-28 Activity 3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	
+	int size, i;
+	int* evenNumbers;
+	
+	printf("Enter size: ");
+	scanf("%d", &size);
+	
+	int arr[size];
+	
+	printf("Enter the values: ");
+	
+	for(i = 0; i < size; i++){
+		scanf("%d", &arr[i]);
+	}
+	
+	printf("Even numbers in the array: ");
+	
+	evenNumbers = getEven(arr, size);
+	
+	return 0;
+}
+
+int* getEven(int* arr, int size){
+	int count = 0;
+	for(int i = 0; i < size; i++){
+		if(arr[i] % 2 == 0){
+			count++;
+		}
+	}
+	
+	int* arr = (int*)malloc(sizeof(arr) * size);
+	
+	    for(int i = 0; i < size; i++){
+        if(arr[i] % 2 == 0){
+            printf("%d ", x[i]);
+        }
+    }
+	
+}
